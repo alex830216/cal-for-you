777d88b24ac9aac9591bf6aa3a455468bce6c16f
Alex <alex830216@gmail.com>
2023-03-21 14:36:54 +0800

    adjust payment width

--- src/views/front/PaymentView.vue
+++ src/views/front/PaymentView.vue
@@ -33,37 +33,67 @@
     </div>
   </section>
 
-  <section class="container py-3">
-    <div class="bg-primary-subtle p-4">
-      <h3 class="pb-3">
+  <section class="container py-3 d-flex justify-content-center bg-primary-exlight">
+    <div class="w-35">
+      <h3 class="pb-3 text-center">
         付款資料
       </h3>
-      <form>
+      <VForm class="pb-4" ref="form" v-slot="{ errors, meta }" @submit="createPayment">
         <div class="mb-3 text-danger">* 為必填</div>
         <div class="mb-3">
-          <label for="credit-account" class="form-label">信用卡帳號 <span class="text-danger">*</span></label>
+          <label for="creditAccount" class="form-label">信用卡帳號 <span class="text-danger">*</span></label>
           <!-- 製做一個 input 欄位，當使用者輸入四個數字就自動加上 -，用 Vue 做 -->
-          <!-- validate max 16 碼 -->
-          <input type="text" class="form-control" id="credit-account" placeholder="xxxx xxxx xxxx xxxx">
+          <VField
+            id="creditAccount"
+            name="信用卡帳號"
+            type="text"
+            class="form-control"
+            :class="{ 'is-invalid': errors['信用卡帳號'] }"
+            placeholder="xxxx-xxxx-xxxx-xxxx"
+            rules="required"
+            v-model="form.creditAccount"
+            maxlength="16"
+          ></VField>
+          <ErrorMessage name="信用卡帳號" class="invalid-feedback"></ErrorMessage>
         </div>
         <div class="mb-3">
-          <label for="validate-date" class="form-label">有效日期 <span class="text-danger">*</span></label>
+          <label for="validateDate" class="form-label">有效日期 <span class="text-danger">*</span></label>
           <!-- 製做一個 input 欄位，當使用者輸入兩個數字就自動加上 /，用 Vue 做 -->
-          <input type="text" class="form-control" id="validate-date" placeholder="07/35">
+          <VField
+            id="validateDate"
+            name="有效日期"
+            type="month"
+            class="form-control"
+            :class="{ 'is-invalid': errors['有效日期'] }"
+            rules="required"
+            v-model="form.validateDate"
+            :min="minDate"
+          ></VField>
+          <ErrorMessage name="有效日期" class="invalid-feedback"></ErrorMessage>
         </div>
         <div class="mb-4">
-          <label for="safe-code" class="form-label">安全碼 <span class="text-danger">*</span></label>
-          <!-- validate max 4 碼 -->
-          <input type="text" class="form-control" id="safe-code" placeholder="123">
+          <label for="safeCode" class="form-label">安全碼 <span class="text-danger">*</span></label>
+          <VField
+            id="safeCode"
+            name="安全碼"
+            type="text"
+            class="form-control"
+            :class="{ 'is-invalid': errors['安全碼'] }"
+            placeholder="123"
+            rules="required|max:4"
+            v-model="form.safeCode"
+            maxlength="4"
+          ></VField>
+          <ErrorMessage name="安全碼" class="invalid-feedback"></ErrorMessage>
         </div>
         <div class="mb-4 fw-bold fs-5">
           商品總金額${{ total }} 元
         </div>
         <div>
           <RouterLink to="/orderInformation" class="btn btn-light me-3">返回填寫訂購資料</RouterLink>
-          <button type="button" class="btn btn-primary" @click="pay">付款</button>
+          <button type="submit" class="btn btn-primary" :disabled="!meta.valid">付款</button>
         </div>
-      </form>
+      </VForm>
     </div>
   </section>
 </template>
@@ -74,7 +104,12 @@ const { VITE_URL, VITE_PATH } = import.meta.env
 export default {
   data () {
     return {
-      total: 0
+      total: 0,
+      form: {
+        creditAccount: '',
+        validateDate: null,
+        safeCode: ''
+      }
     }
   },
   methods: {
@@ -85,12 +120,21 @@ export default {
           this.total = res.data.order.total
         })
     },
-    pay () {
+    createPayment () {
       const orderId = this.$route.query.id
       this.$http.post(`${VITE_URL}/v2/api/${VITE_PATH}/pay/${orderId}`)
         .then(() => {
+          this.$refs.form.resetForm()
           this.$router.push({ name: 'orderCompleted', query: { orderId } })
         })
+    }
+  },
+  computed: {
+    minDate () {
+      const now = new Date()
+      const year = now.getFullYear()
+      const month = now.getMonth() + 1 // JS 月份是從 0 開始計算
+      return `${year}-${month.toString().padStart(2, '0')}`
     }
   },
   mounted () {
