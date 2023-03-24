7f89ee4632f9b152143a1d6bc8f368a63065a36e
Alex <alex830216@gmail.com>
2023-03-24 15:02:43 +0800

    add swiper in home & product

--- src/views/front/HomeView.vue
+++ src/views/front/HomeView.vue
@@ -1,90 +1,124 @@
 <template>
-  <section class="position-relative">
-    <div class="banner d-flex align-items-center justify-content-center fs-1 text-white text-opacity-75"
-    style="background: no-repeat top/cover url(https://images.unsplash.com/photo-1564836235910-c3055ca0f912?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80); width: 100%; height: 100vh;">
-      <div class="container position-absolute top-50 start-50 translate-middle text-center">
-        <p class="fw-bold text-primary-light">不知道該吃多少嗎?<br>交給我們幫你算</p>
-        <RouterLink to="/calculate" class="btn btn-primary-transparent btn-lg">幫你算</RouterLink>
-      </div>
-    </div>
-  </section>
-
-  <section class="text-primary-dark bg-primary-exlight">
-    <div class="container text-center py-5">
-      <div class="fs-2 fw-bold pb-5">您是否有以下困擾?</div>
-      <div class="row align-items-center justify-content-center py-sm">
-        <div class="col-md-6 pb-5 pb-md-0">
-          <img src="https://images.unsplash.com/photo-1511909525232-61113c912358?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1112&q=80" class="img-fluid" alt="diet" style="object-fit: cover;">
-        </div>
-        <div class="col-md-6 ">
-          <h2 class="fs-2">想減肥<br>但不知道該吃哪些食物</h2>
+  <div>
+    <section class="position-relative">
+      <div class="banner d-flex align-items-center justify-content-center fs-1 text-white text-opacity-75"
+      style="background: no-repeat top/cover url(https://images.unsplash.com/photo-1564836235910-c3055ca0f912?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80); width: 100%; height: 100vh;">
+        <div class="container position-absolute top-50 start-50 translate-middle text-center">
+          <p class="fw-bold text-primary-light">不知道該吃多少嗎?<br>交給我們幫你算</p>
+          <RouterLink to="/calculate" class="btn btn-primary-transparent btn-lg">幫你算</RouterLink>
         </div>
       </div>
-      <div class="row flex-row-reverse align-items-center justify-content-center py-sm">
-        <div class="col-md-6 pb-5 pb-md-0">
-          <img src="https://images.unsplash.com/photo-1577563612200-a95560e2edec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" class="img-fluid" alt="obesity" style="object-fit: cover;">
+    </section>
+    <section class="text-primary-dark bg-primary-exlight">
+      <div class="container text-center py-5">
+        <div class="fs-2 fw-bold pb-5">您是否有以下困擾?</div>
+        <div class="row align-items-center justify-content-center py-sm">
+          <div class="col-md-6 pb-5 pb-md-0">
+            <img src="https://images.unsplash.com/photo-1511909525232-61113c912358?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1112&q=80" class="img-fluid" alt="diet" style="object-fit: cover;">
+          </div>
+          <div class="col-md-6 ">
+            <h2 class="fs-2">想減肥<br>但不知道該吃哪些食物</h2>
+          </div>
         </div>
-        <div class="col-md-6">
-          <h2 class="fs-2">想減脂<br>害怕攝取過多熱量</h2>
+        <div class="row flex-row-reverse align-items-center justify-content-center py-sm">
+          <div class="col-md-6 pb-5 pb-md-0">
+            <img src="https://images.unsplash.com/photo-1577563612200-a95560e2edec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" class="img-fluid" alt="obesity" style="object-fit: cover;">
+          </div>
+          <div class="col-md-6">
+            <h2 class="fs-2">想減脂<br>害怕攝取過多熱量</h2>
+          </div>
         </div>
-      </div>
-      <div class="row align-items-center justify-content-center py-sm">
-        <div class="col-md-6 pb-5 pb-md-0">
-          <img src="https://images.unsplash.com/photo-1594737625785-a6cbdabd333c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" class="img-fluid" alt="training" style="object-fit: cover;">
-        </div>
-        <div class="col-md-6">
-          <h2 class="fs-2">想增肌<br>這樣吃，蛋白質夠嗎</h2>
-        </div>
-      </div>
-    </div>
-  </section>
-
-  <section class="position-relative">
-    <div class="concept d-flex align-items-center justify-content-center text-primary-light"
-    style="background: no-repeat center / cover url(https://images.unsplash.com/photo-1505744386214-51dba16a26fc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1306&q=80); width: 100%; height: 300px;">
-      <div class="container position-absolute top-50 start-50 translate-middle text-center">
-        <h2 class="pb-3 fs-2">品牌理念</h2>
-        <p class="fs-4">結合 <strong class="fs-3">Cal</strong>culate 與 <strong class="fs-3">Cal</strong>orie<br>為您計算一天所需的熱量以及蛋白質</p>
-      </div>
-    </div>
-  </section>
-
-  <section class="bg-primary-exlight">
-    <div class="container d-flex flex-column align-items-center justify-content-center text-center py-5">
-      <h2 class="fw-bold pb-4">優選好食</h2>
-      <div class="row g-4">
-        <div v-for="product in products.slice(0, 3)" :key="product.id" class="col-md-6 col-lg-4">
-          <div class="card">
-            <RouterLink :to="`/product/${product.id}`" class="nav-link text-primary-light overlay-link"><img :src="product.imageUrl" class="card-img-top object-cover" height="200" alt="產品圖片"></RouterLink>
-            <div class="card-body">
-              <h5 class="card-title fw-bold">{{ product.title }}</h5>
-              <p class="card-text"><span class="text-decoration-line-through">原價 ${{ product.origin_price }}</span><br>現在只要 ${{ product.price }}</p>
-              <p>熱量：{{ product.calorie }} 大卡</p>
-              <p>蛋白質：{{ product.protein }} 克</p>
-            </div>
+        <div class="row align-items-center justify-content-center py-sm">
+          <div class="col-md-6 pb-5 pb-md-0">
+            <img src="https://images.unsplash.com/photo-1594737625785-a6cbdabd333c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" class="img-fluid" alt="training" style="object-fit: cover;">
+          </div>
+          <div class="col-md-6">
+            <h2 class="fs-2">想增肌<br>這樣吃，蛋白質夠嗎</h2>
           </div>
         </div>
       </div>
-    </div>
-  </section>
-
-  <section class="bg-light text-primary-dark py-4">
-    <div class="container d-flex flex-column align-items-center justify-content-center text-center">
-      <h2>Cal For You 幫你算</h2>
-      <p>聯絡我們：0912345678 <a href="#"><i class="bi bi-facebook fs-3"></i></a> <a href="#" class="line-link"><i class="bi bi-line fs-3"></i></a><br>營業時間（六日公休）：週一 ~ 週五 09：00 ~ 20：00</p>
-    </div>
-  </section>
+    </section>
+    <section class="position-relative">
+      <div class="concept d-flex align-items-center justify-content-center text-primary-light"
+      style="background: no-repeat center / cover url(https://images.unsplash.com/photo-1505744386214-51dba16a26fc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1306&q=80); width: 100%; height: 300px;">
+        <div class="container position-absolute top-50 start-50 translate-middle text-center">
+          <h2 class="pb-3 fs-2">品牌理念</h2>
+          <p class="fs-4">結合 <strong class="fs-3">Cal</strong>culate 與 <strong class="fs-3">Cal</strong>orie<br>為您計算一天所需的熱量以及蛋白質</p>
+        </div>
+      </div>
+    </section>
+    <section class="bg-primary-exlight">
+      <div class="container d-flex flex-column align-items-center justify-content-center text-center py-5">
+        <h2 class="fw-bold pb-4">優選好食</h2>
+        <swiper
+          :navigation="true"
+          :effect="'coverflow'"
+          :grabCursor="true"
+          :loop="true"
+          :coverflowEffect="{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }"
+          :scrollbar="{
+            hide: true,
+          }"
+          :autoplay="{
+            delay: 2500,
+            disableOnInteraction: false,
+          }"
+          :spaceBetween="10"
+          :breakpoints="{
+            '768': {
+              slidesPerView: 3,
+              spaceBetween: 40,
+            }
+          }"
+          :modules="modules"
+          class="swiper"
+        >
+          <swiper-slide v-for="product in products" :key="product.id">
+            <div class="card">
+              <RouterLink :to="`/product/${product.id}`" class="nav-link text-primary-light overlay-link"><img :src="product.imageUrl" class="card-img-top object-cover" height="200" alt="產品圖片"></RouterLink>
+              <div class="card-body">
+                <h5 class="card-title fw-bold">{{ product.title }}</h5>
+                <p class="card-text"><span class="text-decoration-line-through">原價 ${{ product.origin_price }}</span><br>現在只要 ${{ product.price }}</p>
+                <p>熱量：{{ product.calorie }} 大卡</p>
+                <p>蛋白質：{{ product.protein }} 克</p>
+              </div>
+            </div>
+          </swiper-slide>
+        </swiper>
+      </div>
+    </section>
+    <section class="bg-light text-primary-dark py-4">
+      <div class="container d-flex flex-column align-items-center justify-content-center text-center">
+        <h2>Cal For You 幫你算</h2>
+        <p>聯絡我們：0912345678 <a href="#"><i class="bi bi-facebook fs-3"></i></a> <a href="#" class="line-link"><i class="bi bi-line fs-3"></i></a><br>營業時間（六日公休）：週一 ~ 週五 09：00 ~ 20：00</p>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
 import { RouterLink } from 'vue-router'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { Autoplay, Navigation, Scrollbar, EffectCoverflow } from 'swiper'
+
+import 'swiper/css'
+import 'swiper/css/scrollbar'
+import 'swiper/css/effect-coverflow'
+import 'swiper/css/navigation'
 
 const { VITE_URL, VITE_PATH } = import.meta.env
 
 export default {
   data () {
     return {
-      products: []
+      products: [],
+      modules: [Autoplay, Navigation, Scrollbar, EffectCoverflow]
     }
   },
   methods: {
@@ -96,7 +130,9 @@ export default {
     }
   },
   components: {
-    RouterLink
+    RouterLink,
+    Swiper,
+    SwiperSlide
   },
   mounted () {
     this.getProducts()
--- src/views/front/ProductView.vue
+++ src/views/front/ProductView.vue
@@ -1,79 +1,73 @@
 <template>
-  <section class="container pt-6 pb-5">
-    <div style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
-      <ol class="breadcrumb">
-        <li class="breadcrumb-item"><RouterLink to="/products" class="link-primary text-decoration-none">商品列表</RouterLink></li>
-        <li class="breadcrumb-item active" aria-current="page">{{ product.title }}</li>
-      </ol>
-    </div>
-  </section>
-
-  <section class="container pb-7">
-    <div class="row pt-2">
-      <div class="col-12 col-lg-6 text-center">
-        <img :src="product.imageUrl" alt="" class="object-cover" height="450">
+  <div>
+    <section class="container pt-6 pb-5">
+      <div style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><RouterLink to="/products" class="link-primary text-decoration-none">商品列表</RouterLink></li>
+          <li class="breadcrumb-item active" aria-current="page">{{ product.title }}</li>
+        </ol>
       </div>
-      <div class="col-12 col-lg-6 px-5">
-        <h2 class="text-center pb-3">{{ product.title }}</h2>
-        <p>{{ product.description }}</p>
-        <div class="pb-">
-          價格：${{ product.price }}
+    </section>
+    <section class="container pb-7">
+      <div class="row pt-2">
+        <div class="col-12 col-lg-6 text-center pb-5 pb-lg-0">
+          <img :src="product.imageUrl" alt="" class="object-cover" height="450">
         </div>
-        <div>
-          數量<br>
-          <select class="form-select" aria-label="exercise-frequency" v-model="qty">
-            <option :value="i" v-for="i in 10" :key="`${i}18645`">{{ i }}</option>
-          </select>
-        </div>
-        <div class="pt-5 d-flex justify-content-between">
-          <button type="button" class="btn btn-outline-primary mb-0 mb-md-2 mb-lg-0 me-2 me-md-0" @click="add(product)"><i class="bi bi-heart-fill"></i> 加入收藏</button>
-          <button type="button" class="btn btn-primary" @click="addToCart(product.id, qty);showToast()">
-            <i class="bi bi-cart-fill"></i> 加入購物車
-          </button>
-          <ShowCartToast ref="showCartToast"></ShowCartToast>
-        </div>
-      </div>
-    </div>
-  </section>
-
-  <!-- <section class="container d-flex flex-column align-items-center justify-content-center text-center py-6">
-    <h3 class="fw-bold pb-5">您可能也喜歡</h3>
-    <div class="row row-cols-1 row-cols-md-3 g-4">
-      <div class="col">
-        <div class="card h-100">
-          <a href="#"><img src="..." class="card-img-top" alt="..."></a>
-          <div class="card-body">
-            <h5 class="card-title fw-bold">產品名稱</h5>
-            <p class="card-text">原價<br>現在只要</p>
-            <a href="#" class="btn btn-outline-primary mb-0 mb-md-2 mb-lg-0 me-2 me-md-0"><i class="bi bi-heart-fill"></i> 加入收藏</a>
-            <a href="#" class="btn btn-primary"><i class="bi bi-cart-fill"></i> 加入購物車</a>
+        <div class="col-12 col-lg-6 px-5">
+          <h2 class="text-center pb-3">{{ product.title }}</h2>
+          <p>{{ product.description }}</p>
+          <div class="pb-">
+            價格：${{ product.price }}
           </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card h-100">
-          <a href="#"><img src="..." class="card-img-top" alt="..."></a>
-          <div class="card-body">
-            <h5 class="card-title fw-bold">產品名稱</h5>
-            <p class="card-text">原價<br>現在只要</p>
-            <a href="#" class="btn btn-outline-primary mb-0 mb-md-2 mb-lg-0"><i class="bi bi-heart-fill"></i> 加入收藏</a>
-            <a href="#" class="btn btn-primary"><i class="bi bi-cart-fill"></i> 加入購物車</a>
+          <div>
+            數量<br>
+            <select class="form-select" aria-label="exercise-frequency" v-model="qty">
+              <option :value="i" v-for="i in 10" :key="`${i}18645`">{{ i }}</option>
+            </select>
           </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card h-100">
-          <a href="#"><img src="..." class="card-img-top" alt="..."></a>
-          <div class="card-body">
-            <h5 class="card-title fw-bold">產品名稱</h5>
-            <p class="card-text">原價<br>現在只要</p>
+          <div class="pt-5 d-flex justify-content-between">
             <button type="button" class="btn btn-outline-primary mb-0 mb-md-2 mb-lg-0 me-2 me-md-0" @click="add(product)"><i class="bi bi-heart-fill"></i> 加入收藏</button>
-            <a href="#" class="btn btn-primary"><i class="bi bi-cart-fill"></i> 加入購物車</a>
+            <button type="button" class="btn btn-primary" @click="addToCart(product.id, qty);showToast()">
+              <i class="bi bi-cart-fill"></i> 加入購物車
+            </button>
+            <ShowCartToast ref="showCartToast"></ShowCartToast>
           </div>
         </div>
       </div>
-    </div>
-  </section> -->
+    </section>
+    <section class="bg-primary-exlight">
+      <div class="container d-flex flex-fill flex-column align-items-center justify-content-center text-center py-5 col-8 col-lg-4">
+        <h3 class="fw-bold pb-4">您可能也喜歡</h3>
+        <swiper
+          :navigation="true"
+          :grabCursor="true"
+          :loop="true"
+          :scrollbar="{
+            hide: true,
+          }"
+          :autoplay="{
+            delay: 2500,
+            disableOnInteraction: false,
+          }"
+          :spaceBetween="10"
+          :modules="modules"
+          class="swiper"
+        >
+          <swiper-slide v-for="product in products" :key="product.id">
+            <div class="card">
+              <RouterLink :to="`/product/${product.id}`" class="nav-link text-primary-light overlay-link"><img :src="product.imageUrl" class="card-img-top object-cover" height="200" alt="產品圖片"></RouterLink>
+              <div class="card-body">
+                <h5 class="card-title fw-bold">{{ product.title }}</h5>
+                <p class="card-text"><span class="text-decoration-line-through">原價 ${{ product.origin_price }}</span><br>現在只要 ${{ product.price }}</p>
+                <p>熱量：{{ product.calorie }} 大卡</p>
+                <p>蛋白質：{{ product.protein }} 克</p>
+              </div>
+            </div>
+          </swiper-slide>
+        </swiper>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -82,14 +76,23 @@ import { RouterLink } from 'vue-router'
 import cartStore from '@/stores/cart'
 import favoritesStore from '@/stores/favorites'
 import ShowCartToast from '@/components/ShowCartToast.vue'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { Autoplay, Navigation, Scrollbar } from 'swiper'
+
+import 'swiper/css'
+import 'swiper/css/scrollbar'
+import 'swiper/css/effect-coverflow'
+import 'swiper/css/navigation'
 
 const { VITE_URL, VITE_PATH } = import.meta.env
 
 export default {
   data () {
     return {
+      products: [],
       product: {},
-      qty: 1
+      qty: 1,
+      modules: [Autoplay, Navigation, Scrollbar]
     }
   },
   methods: {
@@ -98,6 +101,15 @@ export default {
       this.$http(`${VITE_URL}/v2/api/${VITE_PATH}/product/${id}`)
         .then(res => {
           this.product = res.data.product
+          this.getSameCategory(this.product.category)
+        })
+    },
+    getSameCategory (category) {
+      this.$http(`${VITE_URL}/v2/api/${VITE_PATH}/products/all`)
+        .then(res => {
+          this.products = res.data.products.filter((item) => {
+            return item.category === category
+          })
         })
     },
     showToast () {
@@ -111,7 +123,9 @@ export default {
   },
   components: {
     RouterLink,
-    ShowCartToast
+    ShowCartToast,
+    Swiper,
+    SwiperSlide
   },
   mounted () {
     this.getProduct()
