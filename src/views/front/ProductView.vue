55f0d8d50031aa8042f39547eabf27da48395f66
Alex <alex830216@gmail.com>
2023-04-19 14:31:32 +0800

    change 您可能也喜歡 swiper per slide to 3

--- src/views/front/ProductView.vue
+++ src/views/front/ProductView.vue
@@ -8,7 +8,7 @@
         </ol>
       </div>
     </section>
-    <section class="container pb-7">
+    <section class="container pb-5">
       <div class="row pt-2">
         <div class="col-12 col-lg-6 text-center pb-5 pb-lg-0">
           <swiper
@@ -52,12 +52,35 @@
         </div>
       </div>
     </section>
+    <section class="container pb-5">
+      <ul class="nav nav-tabs mb-4" id="myTab" role="tablist">
+        <li class="nav-item" role="presentation">
+          <button class="nav-link active" id="exchange-tab" data-bs-toggle="tab" data-bs-target="#exchange-and-return" type="button" role="tab" aria-controls="exchange-and-return" aria-selected="true">退換貨須知</button>
+        </li>
+        <li class="nav-item" role="presentation">
+          <button class="nav-link" id="shipping-tab" data-bs-toggle="tab" data-bs-target="#shipping-policy" type="button" role="tab" aria-controls="shipping-policy" aria-selected="false">運送服務</button>
+        </li>
+      </ul>
+      <div class="tab-content" id="myTabContent">
+        <ol class="list-group-numbered tab-pane fade show active" id="exchange-and-return" role="tabpanel" aria-labelledby="exchange-tab" tabindex="0">
+          <li class="list-group-item pb-3">產品在購買後30天內，如因產品本身質量問題、運輸過程中損壞等原因需退換貨，請您聯繫我們客服人員協助處理。如超過30天，我們將無法受理退換貨申請。</li>
+          <li class="list-group-item pb-3">若因您個人原因（如不喜歡、尺寸不合適等）需申請退換貨，請您確保商品完整無損、未經使用、且保持原包裝。我們會依據實際情況收取相應的退換貨運費和處理費。</li>
+          <li class="list-group-item pb-3">退換貨前，請先聯繫我們的客服人員，提供訂單號碼和退換貨原因。我們將根據實際情況為您提供最佳的解決方案。</li>
+          <li class="list-group-item pb-3">在產品退回後，我們會進行檢查確認，確認無誤後會進行退款或換貨。退款將在收到退貨商品後5個工作日內退回原支付賬戶，換貨將在確認退貨商品無誤後5個工作日內發貨。</li>
+          <li class="list-group-item pb-3">如果您對產品的質量有任何疑問或問題，請隨時聯繫我們的客服人員，我們將竭盡所能為您提供滿意的解決方案。</li>
+        </ol>
+        <ol class="list-group-numbered tab-pane fade" id="shipping-policy" role="tabpanel" aria-labelledby="shipping-tab" tabindex="0">
+          <li class="list-group-item pb-3">我們的產品通常會在訂單確認後2-3個工作日內安排發貨。如需加急發貨，請您聯繫我們客服人員，我們將盡可能為您安排快速發貨。</li>
+          <li class="list-group-item pb-3">我們會盡力確保產品在運輸過程中的安全，但是在運輸過程中出現的損壞、延誤或丟失等情況我們將不承擔責任。如有此類問題發生，請及時聯繫我們的客服人員，我們將盡力協助處理。</li>
+          <li class="list-group-item pb-3">如果您對運送服務有任何疑問或問題，請隨時聯繫我們的客服人員，我們將竭盡所能為您提供滿意的解決方案。</li>
+        </ol>
+      </div>
+    </section>
     <section class="bg-primary-exlight">
-      <div class="container d-flex flex-fill flex-column align-items-center justify-content-center text-center py-5 col-8 col-lg-4">
+      <div class="container d-flex flex-fill flex-column align-items-center justify-content-center text-center py-5 col-8">
         <h3 class="fw-bold pb-4">您可能也喜歡</h3>
         <swiper
-          :spaceBetween="30"
-          :centeredSlides="true"
+          :spaceBetween="10"
           :autoplay="{
             delay: 2500,
             disableOnInteraction: false,
@@ -66,6 +89,13 @@
             hide: true,
           }"
           :navigation="true"
+          :slidesPerView="1"
+            :breakpoints="{
+              '992': {
+                slidesPerView: 3,
+                spaceBetween: 30,
+              }
+            }"
           :modules="modules"
           class="swiper">
           <swiper-slide class="bg-primary-exlight" v-for="categoryProduct in products" :key="categoryProduct.id">
